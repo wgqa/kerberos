7c485715f9963f1783c5e69d882aaefe98c15d1f
JunzhengSong <fengchen692@gmail.com>
2022-06-01 15:19:59 +0800

    service thread

--- src/Server/Service.java
+++ src/Server/Service.java
@@ -20,82 +20,24 @@ import static Server.StringToUnicode.stringToUnicode;
 public class Service {
     private static String pubKey="";
     public static void main(String[] args) throws Exception {
-        boolean flag =true;
-        while(flag){
+        try {
+            //1.创建一个服务器端Socket，即ServerSocket，指定绑定的端口，并监听此端口
             ServerSocket serverSocket = new ServerSocket(1231);
-
+            InetAddress address = InetAddress.getLocalHost();
+            String ip = address.getHostAddress();
             Socket socket = null;
             //2.调用accept()等待客户端连接
-
-            socket = serverSocket.accept();
-
-            Socket s = socket;
-
-            //3.连接后获取输入流，读取客户端信息
-            InputStream is=null;
-            InputStreamReader isr=null;
-            BufferedReader br=null;
-            OutputStream os=null;
-            PrintWriter pw=null;
-            is = socket.getInputStream();     //获取输入流
-            isr = new InputStreamReader(is,"UTF-8");
-            br = new BufferedReader(isr);
-
-
-            String messageFromClient =null;
-            String messageToClient="";
-
-            String[] messageFromClient1 =null;
-            String[] messageFromClient2 =null;
-
-            DBconnect db = new DBconnect();
-            Statement stat = db.connect().createStatement();
-
-            while((messageFromClient=br.readLine())!=null){//循环读取客户端的信息
-                System.out.println("客户端发送过来的信息" + messageFromClient);
-
-                 if(messageFromClient.charAt(0)=='1'){
-                     messageFromClient=messageFromClient.substring(1,messageFromClient.length());
-                     messageToClient=db.selectByName(stat,messageFromClient);
-                 }
-
-                if(messageFromClient.charAt(0)=='2'){
-                    messageFromClient=messageFromClient.substring(1,messageFromClient.length());
-                    messageToClient=db.selectByPublisher(stat,messageFromClient);
-                }
-
-                if(messageFromClient.charAt(0)=='3'){
-                    messageFromClient=messageFromClient.substring(1,messageFromClient.length());
-                    messageToClient=db.selectByAuthor(stat,messageFromClient);
-                }
-
-                if(messageFromClient.charAt(0)=='4'){
-                    pubKey=messageFromClient.substring(1,messageFromClient.length());
-                }
-
-                if(messageToClient.equals("")){
-                    System.out.println("没找符合要求的书");
-                    messageToClient="没找符合要求的书";
-                }
-                System.out.println("发送的信息："+messageToClient);
-                //messageToClient=gbEncoding(messageToClient);
-                //RSAPublicKey pubKey = LIBRARY
-                messageToClient = encryptByPublicKey(messageToClient, pubkey3);
-                OutputStream outputStream = socket.getOutputStream();
-                socket.getOutputStream().write(messageToClient.getBytes("UTF-8"));
+            System.out.println("~~~Server服务端已就绪，等待客户端接入~，服务端ip地址: " + "192.198.43.233");
+            while(true){
+                //调用accept()方法开始监听，等待客户端的连接
+                socket=serverSocket.accept();
+                //创建一个新的线程
+                ServiceThread serviceThread=new ServiceThread(socket);
+                //启动线程
+                serviceThread.start();
             }
-//            if(messageFromClient.equals("exit")){
-//                flag=false;
-//            }
-            //发送
-            // 建立连接后获得输出流
-
-
-
-            //messageFromClient=br.readLine();
-            serverSocket.close();
-            socket.shutdownInput();//关闭输入流
-            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
---
+++ src/Server/ServiceThread.java
@@ -0,0 +1,86 @@
+package Server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.Statement;
+
+import static RSA.RSAUtils.encryptByPublicKey;
+import static RSA.getKey.pubkey3;
+
+public class ServiceThread extends Thread{
+
+    // 和本线程相关的Socket
+    Socket socket = null;
+
+    public ServiceThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run(){
+        InputStream is=null;
+        InputStreamReader isr=null;
+        BufferedReader br=null;
+        OutputStream os=null;
+        PrintWriter pw=null;
+        try {
+            is = socket.getInputStream();     //获取输入流
+            isr = new InputStreamReader(is,"UTF-8");
+            br = new BufferedReader(isr);
+
+
+            String messageFromClient =null;
+            String messageToClient="";
+
+            String[] messageFromClient1 =null;
+            String[] messageFromClient2 =null;
+
+            DBconnect db = new DBconnect();
+            Statement stat = db.connect().createStatement();
+
+            while((messageFromClient=br.readLine())!=null){//循环读取客户端的信息
+                System.out.println("客户端发送过来的信息" + messageFromClient);
+
+                if(messageFromClient.charAt(0)=='1'){
+                    messageFromClient=messageFromClient.substring(1,messageFromClient.length());
+                    messageToClient=db.selectByName(stat,messageFromClient);
+                }
+
+                if(messageFromClient.charAt(0)=='2'){
+                    messageFromClient=messageFromClient.substring(1,messageFromClient.length());
+                    messageToClient=db.selectByPublisher(stat,messageFromClient);
+                }
+
+                if(messageFromClient.charAt(0)=='3'){
+                    messageFromClient=messageFromClient.substring(1,messageFromClient.length());
+                    messageToClient=db.selectByAuthor(stat,messageFromClient);
+                }
+
+
+
+                if(messageToClient.equals("")){
+                    System.out.println("没找符合要求的书");
+                    messageToClient="没找符合要求的书";
+                }
+                System.out.println("发送的信息："+messageToClient);
+                //messageToClient=gbEncoding(messageToClient);
+                //RSAPublicKey pubKey = LIBRARY
+                messageToClient = encryptByPublicKey(messageToClient, pubkey3);
+                OutputStream outputStream = socket.getOutputStream();
+                socket.getOutputStream().write(messageToClient.getBytes("UTF-8"));
+            }
+//            if(messageFromClient.equals("exit")){
+//                flag=false;
+//            }
+            //发送
+            // 建立连接后获得输出流
+
+
+            //messageFromClient=br.readLine();
+            socket.shutdownInput();//关闭输入流
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
