74045533f2e3d8e7713b8ec25abcfa54bf77517e
wgqa <2532092540@qq.com>
2022-06-01 08:48:01 +0800

    xiugai

--- src/RSA/RSA.java
+++ src/RSA/RSA.java
@@ -10,29 +10,24 @@ public class RSA {
 		int M1[] = new int[M.length];
 		String tmp = new String();
 
-		String s ="";  //&#xFFFD;&#xFFFD;&#xFFFD;&#x0436;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#x01B5;&#xFFFD;&#xFFFD;&#x06FC;&#xFFFD;
+		String s ="";  
 		for(int i=0;i<M.length;i++)
 		{
-			if (Character.isDigit(M[i])){  // &#xFFFD;&#x0436;&#xFFFD;&#xFFFD;&#x01F7;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;
+			if (Character.isDigit(M[i])){
 				M1[i] = Integer.parseInt(String.valueOf(M[i]));
 			}
 			else {
-				System.err.println("String&#x05EA;Binary&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;");
+				System.err.println("String含有非数字类型");
 			}
 
 			tmp = supplement(4, Integer.toBinaryString(M1[i]));
-			//ÿ&#x04BB;&#x03BB;&#xFFFD;&#xFFFD;&#x05EA;&#xFFFD;&#xFFFD;&#xFFFD;&#x02F6;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;
-			s = s + tmp; //&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;string&#xFFFD;&#xFFFD;
+
+			s = s + tmp;
 		}
 		return s;
 	}
 
-	/**
-	 * &#xFFFD;&#xFFFD;str&#xFFFD;&#xFFFD;&#xFFFD;&#xBD7D;n&#x03BB;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#x03BB;&#x0434;0
-	 * @param n
-	 * @param str &#x04AA;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#x05B7;&#xFFFD;&#xFFFD;&#xFFFD;
-	 * @return
-	 */
+
 	public static String supplement(int n,String str){
 		if(n>str.length()) {
 			int sl=str.length();//string&#x052D;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;
@@ -49,19 +44,19 @@ public class RSA {
 		int M1[] = new int[M.length];
 		String tmp = new String();
 
-		String s ="";  //&#xFFFD;&#xFFFD;&#xFFFD;&#x0436;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#x01B5;&#xFFFD;&#xFFFD;&#x06FC;&#xFFFD;
+		String s ="";
 		for(int i=0;i<M.length;i++)
 		{
-			if (Character.isDigit(M[i])){  // &#xFFFD;&#x0436;&#xFFFD;&#xFFFD;&#x01F7;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;
+			if (Character.isDigit(M[i])){
 				M1[i] = Integer.parseInt(String.valueOf(M[i]));
 			}
 			else {
-				System.err.println("String&#x05EA;Binary&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;");
+				System.err.println("String里含有非数字的类型");
 			}
 
 			tmp = supplement(3, Integer.toBinaryString(M1[i]));
-			//ÿ&#x04BB;&#x03BB;&#xFFFD;&#xFFFD;&#x05EA;&#xFFFD;&#xFFFD;&#xFFFD;&#x02F6;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;
-			s = s + tmp; //&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;string&#xFFFD;&#xFFFD;
+
+			s = s + tmp;
 		}
 		return s;
 	}
@@ -124,15 +119,9 @@ public class RSA {
 	}
 
 
-/**
- * &#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;
- * @param plainText
- * @param pubkey
- * @return
- */
-	/***************&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#x01F0;&#xFFFD;&#xFFFD;01&#x05EA;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#x02AE;&#xFFFD;&#xFFFD;&#xFFFD;&#x01BC;&#xFFFD;&#xFFFD;&#x0723;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#x073A;&#xFFFD;&#xFFFD;&#xFFFD;&#x05EA;&#xFFFD;&#xFFFD;&#xFFFD;&#x0636;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;***********************/
+
 	public String encrypt(String plainText, String[] pubkey){
-		System.out.println("-----RSA&#xFFFD;&#xFFFD;&#x02BC;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;-----");
+		System.out.println("-----RSA开始加密-----");
 		BigInteger n = new BigInteger(pubkey[0]) ;
 		BigInteger e = new BigInteger(pubkey[1]) ;
 //	plainText = StringToBinary2(plainText);//&#xFFFD;&#x023D;&#xFFFD;&#xFFFD;&#xFFFD;&#x022B;&#xFFFD;&#xFFFD;&#x05EA;&#xFFFD;&#xFFFD;&#xFFFD;&#x0276;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;
@@ -148,15 +137,10 @@ public class RSA {
 		String	C=c.toString(2) ;
 		return C.toString() ;
 	}
-/**
- * &#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;
- * @param cipherText
- * @param selfkey
- * @return
- */
-	/***************&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#x01F0;&#xFFFD;&#xFFFD;01&#x05EA;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#x02AE;&#xFFFD;&#xFFFD;&#xFFFD;&#x01BD;&#xFFFD;&#xFFFD;&#x0723;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#x073A;&#xFFFD;&#xFFFD;&#xFFFD;&#x05EA;&#xFFFD;&#xFFFD;&#xFFFD;&#x0636;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;***********************/
+
+
 	public String decrypt(String cipherText, String[] selfkey){
-		System.out.println("-----RSA&#xFFFD;&#xFFFD;&#x02BC;&#xFFFD;&#xFFFD;&#xFFFD;&#xFFFD;-----");
+		System.out.println("-----RSA开始解密;-----");
 		BigInteger n = new BigInteger(selfkey[0]) ;
 		BigInteger d = new BigInteger(selfkey[1]) ;
 		String c1 = new BigInteger(cipherText,2).toString(10);
